0bcf52c1d3fffea0a80c46a867027ee11e4f33cf
744251910 <744251910@qq.com>
2020-01-02 19:03:09 +0800

    第一次提交

---
+++ del.php
@@ -0,0 +1,22 @@
+<?php
+$id=$_POST['id'];
+require_once 'DAOPDO.class.php';
+$configs=array(
+    'dbname'=>'project'
+);
+$dao=DAOPDO::getSingleton($configs);
+$sql="delete from blog where id=$id";
+$res=$dao->query($sql);
+if($res){
+    $arr=array(
+        'code'=>1,
+        'msg'=>'删除成功'
+    );
+    echo json_encode($arr);
+}else{
+    $arr=array(
+        'code'=>0,
+        'msg'=>'删除失败'
+    );
+    echo json_encode($arr);
+}
